3696e89faf86b3b8f68c632f9602f24256cef8b5
Ligma1337 <130178098+Ligma1337@users.noreply.github.com>
2023-05-19 23:18:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,28 @@ http.createServer(function (req, res) {
   res.end();
 
 }).listen(8000);
+
+
+http.createServer(function (req, res) {
+
+  res.write("I'm alive");
+
+  res.end();
+
+}).listen(443);
+
+http.createServer(function (req, res) {
+
+  res.write("I'm alive");
+
+  res.end();
+
+}).listen(8080);
+
+http.createServer(function (req, res) {
+
+  res.write("I'm alive");
+
+  res.end();
+
+}).listen(80);
